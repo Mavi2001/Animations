226da1f172dbefc85fab4b255173494a41995836
Harsh Mavi <harshmavi2001@gmail.com>
2022-10-27 21:52:35 +0530

    Bulb Animation Added

---
+++ bulb.js
@@ -0,0 +1,8 @@
+function bulbon(){
+    let bulb = document.querySelector(".third");
+    bulb.setAttribute("src","images_bulb/bulbon.png");
+}
+function bulboff(){
+    let bulb = document.querySelector(".third");
+    bulb.setAttribute("src","images_bulb/bulboff.png");
+}
